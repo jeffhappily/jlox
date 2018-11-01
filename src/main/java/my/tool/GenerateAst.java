e8cdcac1fa9efb19c2413f75f01bf55d8ce58170
Jeff Cheah <jeff.cheah@grabtaxi.com>
2018-11-01 23:03:28 +0800

    Add defineType function

--- src/main/java/my/tool/GenerateAst.java
+++ src/main/java/my/tool/GenerateAst.java
@@ -37,4 +37,28 @@ public class GenerateAst {
         writer.println("}");
         writer.close();
     }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("  static class " + className + " extends " + baseName + " {");
+
+        // Constructor.
+        writer.println("    " + className + "(" + fieldList + ") {");
+
+        // Store parameters in fields.
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("      this." + name + " = " + name + ";");
+        }
+
+        writer.println("    }");
+
+        // Fields.
+        writer.println();
+        for (String field : fields) {
+            writer.println("    final " + field + ";");
+        }
+
+        writer.println("  }");
+    }
 }
