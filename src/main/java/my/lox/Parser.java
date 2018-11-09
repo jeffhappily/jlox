0820b732db2871e403cf42db2fa28dec32205705
jeffhappily <goodboy96@live.com.my>
2018-11-09 08:16:19 +0800

    Add parser class

---
+++ src/main/java/my/lox/Parser.java
@@ -0,0 +1,14 @@
+package my.lox;
+
+import java.util.List;
+
+import static my.lox.TokenType.*;
+
+class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+}
