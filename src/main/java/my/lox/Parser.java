e2f45772512427cb330e7aed97b2996c6b9e6c09
jeffhappily <goodboy96@live.com.my>
2018-11-10 18:32:39 +0800

    Add addition and multiplication

--- src/main/java/my/lox/Parser.java
+++ src/main/java/my/lox/Parser.java
@@ -40,6 +40,30 @@ class Parser {
         return expr;
     }
 
+    private Expr addition() {
+        Expr expr = multiplication();
+
+        while (match(MINUS, PLUS)) {
+            Token operator = previous();
+            Expr right = multiplication();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr multiplication() {
+        Expr expr = unary();
+
+        while (match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
     private boolean match(TokenType... types) {
         for (TokenType type : types) {
             if (check(type)) {
