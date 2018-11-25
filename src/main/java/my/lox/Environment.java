583bd3487017bd4bf9856df9c59e9bcc8062a810
jeffhappily <goodboy96@live.com.my>
2018-11-25 18:37:27 +0800

    Assign variable to enclosing if it's not found in current environment

--- src/main/java/my/lox/Environment.java
+++ src/main/java/my/lox/Environment.java
@@ -32,6 +32,11 @@ class Environment {
             return;
         }
 
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
