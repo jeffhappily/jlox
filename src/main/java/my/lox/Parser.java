a53af80b5e8932b85d5b9a659c01f210d2bf0be3
jeffhappily <goodboy96@live.com.my>
2018-11-27 14:38:29 +0800

    Add while statement in parser

--- src/main/java/my/lox/Parser.java
+++ src/main/java/my/lox/Parser.java
@@ -48,6 +48,9 @@ class Parser {
         if (match(PRINT))
             return printStatement();
 
+        if (match(WHILE))
+            return whileStatement();
+
         if (match(LEFT_BRACE))
             return new Stmt.Block(block());
 
@@ -84,6 +87,15 @@ class Parser {
 
         consume(SEMICOLON, "Expect ';' after variable declaration.");
         return new Stmt.Var(name, initializer);
+    }
+
+    private Stmt whileStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'while'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after condition.");
+        Stmt body = statement();
+
+        return new Stmt.While(condition, body);
     }
 
     private Stmt expressionStatement() {
