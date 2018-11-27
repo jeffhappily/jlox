d3de7678c88ad0ef1c854593bf3c9b36ca2bf38c
jeffhappily <goodboy96@live.com.my>
2018-11-27 12:05:35 +0800

    Add if statement to parser

--- src/main/java/my/lox/Parser.java
+++ src/main/java/my/lox/Parser.java
@@ -42,6 +42,9 @@ class Parser {
     }
 
     private Stmt statement() {
+        if (match(IF))
+            return ifStatement();
+
         if (match(PRINT))
             return printStatement();
 
@@ -49,6 +52,20 @@ class Parser {
             return new Stmt.Block(block());
 
         return expressionStatement();
+    }
+
+    private Stmt ifStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'if'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after if condition.");
+
+        Stmt thenBranch = statement();
+        Stmt elseBranch = null;
+        if (match(ELSE)) {
+            elseBranch = statement();
+        }
+
+        return new Stmt.If(condition, thenBranch, elseBranch);
     }
 
     private Stmt printStatement() {
