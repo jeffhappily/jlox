f5b6bda83996082c2a6807c155b5aafaf40d12ae
jeffhappily <goodboy96@live.com.my>
2018-11-28 17:00:38 +0800

    Revert "Add break and continue keyword"
    This reverts commit eb457deacbe987aff2eb010d72cd5e5bc292bc55.

--- src/main/java/my/lox/Scanner.java
+++ src/main/java/my/lox/Scanner.java
@@ -33,8 +33,6 @@ class Scanner {
         keywords.put("true", TRUE);
         keywords.put("var", VAR);
         keywords.put("while", WHILE);
-        keywords.put("break", BREAK);
-        keywords.put("continue", CONTINUE);
     }
 
     Scanner(String source) {
